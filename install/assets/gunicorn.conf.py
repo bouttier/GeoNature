92f2b55164b2e1a6b1149386996511cde4e37372
Élie Bouttier <elie@bouttier.eu>
2026-09-16 21:23:28 +0200

    allows starting GeoNature without config file
    If all required config parameters are provided by env vars, there
    is no needs for a config file.

--- install/assets/gunicorn.conf.py
+++ install/assets/gunicorn.conf.py
@@ -1,5 +1,10 @@
+import os
+
 from geonature.utils.env import CONFIG_FILE
 from geonature.utils.module import iter_modules_config
 
 reload = True
-reload_extra_files = [CONFIG_FILE] + list(iter_modules_config())
+reload_extra_files = []
+if os.path.exists(CONFIG_FILE):
+    reload_extra_files.append(CONFIG_FILE)
+reload_extra_files.extend(iter_modules_config())
